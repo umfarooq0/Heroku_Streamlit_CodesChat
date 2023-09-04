0986b316bed5f369f6df4de335ab06a971f8b193
Usman Farooq <umfarooq0@gmail.com>
2023-09-04 08:13:25 +0100

    update files

--- app.py
+++ app.py
@@ -4,17 +4,26 @@ from llama_index.llms import OpenAI
 import os
 import streamlit as st
 from llama_index import ServiceContext
+from langchain import LLMChain
+from langchain import PromptTemplate
+from langchain.chains import ConversationChain
+from langchain.agents import ZeroShotAgent, AgentExecutor
+from langchain.memory import ConversationBufferMemory
+from langchain import SerpAPIWrapper
+from langchain.chat_models import ChatOpenAI
+from langchain.agents import Tool
+from langchain.chains.combine_documents.stuff import StuffDocumentsChain
+from langchain.chains import SequentialChain
+from langchain.chains import RetrievalQA
+from langchain.agents import ConversationalChatAgent
 
-
+os.environ["SERPAPI_API_KEY"] = "387c83df077ba5cc8533eac5f7365981e84300455a2cafcc77e9f0d9d6ef9795"
 openai_api_key = "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"  
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
 st.title("Codes Chat") 
 
-st.write('''💬 Chatbot
-This chatbot aims to use ChatGPT to provide answers to questions related to the GB Grid Code.
-While ChatGPT has not been 'trained' on the GB Elctricity Codes, it is focused on answering questions on it.
-        
+st.write('''   
 PLEASE do not enter any confidential information. This uses ChatGPT which stores user inputs. 
 We endeavor to use open-source models in the future. 
         
@@ -33,9 +42,104 @@ storage_context = StorageContext.from_defaults(persist_dir='GC')
 
 index = load_index_from_storage(storage_context, service_context=service_context)
 
-chat_engine = index.as_chat_engine(chat_mode="openai", verbose=True)
-#response = chat_engine.chat("reactive power requirements for connections to GB grid")
 
+
+storage_context_STC = StorageContext.from_defaults(persist_dir='STC')
+
+index_STC = load_index_from_storage(storage_context_STC, service_context=service_context)
+
+
+storage_context_SQSS = StorageContext.from_defaults(persist_dir='SQSS')
+
+index_SQSS = load_index_from_storage(storage_context_SQSS, service_context=service_context)
+
+
+#storage_context_CUSC = StorageContext.from_defaults(persist_dir='CUSC')
+#index_CUSC = load_index_from_storage(storage_context_CUSC, service_context=service_context)
+
+
+
+
+prefix = """I want you take the role of a senior power system engineer in the GB. You are responding to questions by junior engineers about compliance and regulation. The audience is going to mainly people
+who are not technical experts but may have some knowledge of the GB electricity grid. Respond in very simple language, explaining all the technical terms as if the audience doesnt know anything. Provide references"""
+suffix = """Begin!"
+
+{chat_history}
+Question: {input}
+{agent_scratchpad}"""
+
+
+prompt_template = """Use the following pieces of context to answer the question at the end.
+If you don't know the answer, please think rationally answer from your own knowledge base
+
+{context}
+
+Question: {question}
+"""
+
+prompt = PromptTemplate(
+    template=prompt_template, input_variables=["context", "question"]
+)
+
+search = SerpAPIWrapper()
+llm = ChatOpenAI(temperature=0.2,model="gpt-3.5-turbo-0613")
+
+tools = [
+    Tool(
+        name="GridCodeQA",
+        func=lambda q: str(index.as_query_engine().query(q)),
+        description="useful for when you want to answer questions about the GridCode.",
+        return_direct=True,
+    ),
+    
+    Tool(
+        name="STC_QA",
+        func= lambda q: str(index_STC.as_query_engine().query(q)),
+        description="useful for when you want to answer questions about STC.",
+        return_direct=True,
+    ),
+    Tool(
+        name="SQSS_QA",
+        func= lambda q: str(index_SQSS.as_query_engine().query(q)),
+        description="useful for when you want to answer questions about the SQSS.",
+        return_direct=True,
+    ),
+    Tool(
+        name="Intermediate Answer",
+        func=search.run,
+        description="useful for when you need understand more context around the question or if you are unable to find the answer in the grid Code",
+    )
+]
+
+
+prompt = ZeroShotAgent.create_prompt(
+    tools,
+    prefix=prefix,
+    suffix=suffix,
+    input_variables=["input", "chat_history", "agent_scratchpad"],
+)
+memory = ConversationBufferMemory(memory_key="chat_history")
+llm_chain = LLMChain(llm=llm, prompt=prompt)
+
+
+system_msg = """I want you take the role of a senior power system engineer in the GB. You are responding to questions by junior engineers about compliance and regulation. The audience is going to mainly people
+who are not technical experts but may have some knowledge of the GB electricity grid. Respond in very simple language, explaining all the technical terms as if the audience doesnt know anything. Provide references"""
+
+agent = ConversationalChatAgent.from_llm_and_tools(
+    llm=llm,
+    tools=tools,
+    system_message=system_msg
+)
+
+agent_chain = AgentExecutor.from_agent_and_tools(
+    agent=agent, tools=tools, verbose=True, memory=ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+)
+
+
+#agent = ZeroShotAgent(llm_chain=llm_chain, tools=tools, verbose=True)
+#agent_chain = AgentExecutor.from_agent_and_tools(
+#    agent=agent, tools=tools, verbose=True, memory=memory
+#)
 
 
 if prompt := st.chat_input('Ask a question about GB Electricity Codes'):
@@ -46,12 +150,12 @@ if prompt := st.chat_input('Ask a question about GB Electricity Codes'):
     with st.chat_message("user"):
         st.markdown(prompt)
     #response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-    response = chat_engine.chat(prompt,function_call="query_engine_tool")
+    response = agent_chain.run(prompt)
 
-    msg = response.response
-    st.session_state.messages.append({"role": "assistant", "content": msg})
+    #msg = response.response
+    st.session_state.messages.append({"role": "assistant", "content": response})
     
     with st.chat_message("assistant"):
-        st.markdown(msg)
+        st.markdown(response)
 
 
